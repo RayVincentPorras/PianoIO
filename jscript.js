ff4fe3483141cd4c63d6ed12b4e9400b604cb7e8
Ray <pliskin1987@gmail.com>
2016-10-09 12:03:33 -0700

    Changed audio file type

--- jscript.js
+++ jscript.js
@@ -55,57 +55,57 @@ var keyToNote = {
     'o':'c3',
 }
 
-var c1 = new Audio("piano/c1.wav");
-var cSharp1 = new Audio("piano/cSharp1.wav");
-var d1 = new Audio("piano/d1.wav");
-var dSharp1 = new Audio("piano/dSharp1.wav");
-var e1 = new Audio("piano/e1.wav");
-var f1 = new Audio("piano/f1.wav");
-var fSharp1 = new Audio("piano/fSharp1.wav");
-var g1 = new Audio("piano/g1.wav");
-var gSharp1 = new Audio("piano/gSharp1.wav");
-var a1 = new Audio("piano/a1.wav");
-var aSharp1 = new Audio("piano/aSharp1.wav");
-var b1 = new Audio("piano/b1.wav");
-var c2 = new Audio("piano/c2.wav");
-var cSharp2 = new Audio("piano/cSharp2.wav");
-var d2 = new Audio("piano/d2.wav");
-var dSharp2 = new Audio("piano/dSharp2.wav");
-var e2 = new Audio("piano/e2.wav");
-var f2 = new Audio("piano/f2.wav");
-var fSharp2 = new Audio("piano/fSharp2.wav");
-var g2 = new Audio("piano/g2.wav");
-var gSharp2 = new Audio("piano/gSharp2.wav");
-var a2 = new Audio("piano/a2.wav");
-var aSharp2 = new Audio("piano/aSharp2.wav");
-var b2 = new Audio("piano/b2.wav");
-var c3 = new Audio("piano/c3.wav");
+var c1 = new Audio("piano/c1.ogg");
+var cSharp1 = new Audio("piano/cSharp1.ogg");
+var d1 = new Audio("piano/d1.ogg");
+var dSharp1 = new Audio("piano/dSharp1.ogg");
+var e1 = new Audio("piano/e1.ogg");
+var f1 = new Audio("piano/f1.ogg");
+var fSharp1 = new Audio("piano/fSharp1.ogg");
+var g1 = new Audio("piano/g1.ogg");
+var gSharp1 = new Audio("piano/gSharp1.ogg");
+var a1 = new Audio("piano/a1.ogg");
+var aSharp1 = new Audio("piano/aSharp1.ogg");
+var b1 = new Audio("piano/b1.ogg");
+var c2 = new Audio("piano/c2.ogg");
+var cSharp2 = new Audio("piano/cSharp2.ogg");
+var d2 = new Audio("piano/d2.ogg");
+var dSharp2 = new Audio("piano/dSharp2.ogg");
+var e2 = new Audio("piano/e2.ogg");
+var f2 = new Audio("piano/f2.ogg");
+var fSharp2 = new Audio("piano/fSharp2.ogg");
+var g2 = new Audio("piano/g2.ogg");
+var gSharp2 = new Audio("piano/gSharp2.ogg");
+var a2 = new Audio("piano/a2.ogg");
+var aSharp2 = new Audio("piano/aSharp2.ogg");
+var b2 = new Audio("piano/b2.ogg");
+var c3 = new Audio("piano/c3.ogg");
 
-var c1Sustained = new Audio("piano/Sustained/c1.wav");
-var cSharp1Sustained = new Audio("piano/Sustained/cSharp1.wav");
-var d1Sustained = new Audio("piano/Sustained/d1.wav");
-var dSharp1Sustained = new Audio("piano/Sustained/dSharp1.wav");
-var e1Sustained = new Audio("piano/Sustained/e1.wav");
-var f1Sustained = new Audio("piano/Sustained/f1.wav");
-var fSharp1Sustained = new Audio("piano/Sustained/fSharp1.wav");
-var g1Sustained = new Audio("piano/Sustained/g1.wav");
-var gSharp1Sustained = new Audio("piano/Sustained/gSharp1.wav");
-var a1Sustained = new Audio("piano/Sustained/a1.wav");
-var aSharp1Sustained = new Audio("piano/Sustained/aSharp1.wav");
-var b1Sustained = new Audio("piano/Sustained/b1.wav");
-var c2Sustained = new Audio("piano/Sustained/c2.wav");
-var cSharp2Sustained = new Audio("piano/Sustained/cSharp2.wav");
-var d2Sustained = new Audio("piano/Sustained/d2.wav");
-var dSharp2Sustained = new Audio("piano/Sustained/dSharp2.wav");
-var e2Sustained = new Audio("piano/Sustained/e2.wav");
-var f2Sustained = new Audio("piano/Sustained/f2.wav");
-var fSharp2Sustained = new Audio("piano/Sustained/fSharp2.wav");
-var g2Sustained = new Audio("piano/Sustained/g2.wav");
-var gSharp2Sustained = new Audio("piano/Sustained/gSharp2.wav");
-var a2Sustained = new Audio("piano/Sustained/a2.wav");
-var aSharp2Sustained = new Audio("piano/Sustained/aSharp2.wav");
-var b2Sustained = new Audio("piano/Sustained/b2.wav");
-var c3Sustained = new Audio("piano/Sustained/c3.wav");
+var c1Sustained = new Audio("piano/Sustained/c1.ogg");
+var cSharp1Sustained = new Audio("piano/Sustained/cSharp1.ogg");
+var d1Sustained = new Audio("piano/Sustained/d1.ogg");
+var dSharp1Sustained = new Audio("piano/Sustained/dSharp1.ogg");
+var e1Sustained = new Audio("piano/Sustained/e1.ogg");
+var f1Sustained = new Audio("piano/Sustained/f1.ogg");
+var fSharp1Sustained = new Audio("piano/Sustained/fSharp1.ogg");
+var g1Sustained = new Audio("piano/Sustained/g1.ogg");
+var gSharp1Sustained = new Audio("piano/Sustained/gSharp1.ogg");
+var a1Sustained = new Audio("piano/Sustained/a1.ogg");
+var aSharp1Sustained = new Audio("piano/Sustained/aSharp1.ogg");
+var b1Sustained = new Audio("piano/Sustained/b1.ogg");
+var c2Sustained = new Audio("piano/Sustained/c2.ogg");
+var cSharp2Sustained = new Audio("piano/Sustained/cSharp2.ogg");
+var d2Sustained = new Audio("piano/Sustained/d2.ogg");
+var dSharp2Sustained = new Audio("piano/Sustained/dSharp2.ogg");
+var e2Sustained = new Audio("piano/Sustained/e2.ogg");
+var f2Sustained = new Audio("piano/Sustained/f2.ogg");
+var fSharp2Sustained = new Audio("piano/Sustained/fSharp2.ogg");
+var g2Sustained = new Audio("piano/Sustained/g2.ogg");
+var gSharp2Sustained = new Audio("piano/Sustained/gSharp2.ogg");
+var a2Sustained = new Audio("piano/Sustained/a2.ogg");
+var aSharp2Sustained = new Audio("piano/Sustained/aSharp2.ogg");
+var b2Sustained = new Audio("piano/Sustained/b2.ogg");
+var c3Sustained = new Audio("piano/Sustained/c3.ogg");
 
 
 var sustain = false;
@@ -132,8 +132,8 @@ $(document).on('keydown', function(event) {
     var note = findNoteFromEvent(event);
     if (note && !playingNotes[note]) {
         playingNotes[note] = true;
-        var noteAudio = new Audio('piano/' + note + '.wav');
-        var noteAudioSustained = new Audio('piano/Sustained/' + note + '.wav');
+        var noteAudio = new Audio('piano/' + note + '.ogg');
+        var noteAudioSustained = new Audio('piano/Sustained/' + note + '.ogg');
 
         if (noteAudio && sustain == true) {
             noteAudioSustained.play();
@@ -149,3 +149,5 @@ $(document).on('keyup', function(event) {
     var note = findNoteFromEvent(event);
     delete playingNotes[note];
 });
+
+
